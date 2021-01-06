165910146ae914a1f66c11e6b4b5f738d4aeab69
sowmyatellapragada <sowmyatellapragada01@gmail.com>
2021-01-06 15:32:31 +0530

    Add project files.

---
+++ DeadLock1/Account.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DeadLock1
+{
+    public class Account
+    {
+        double Balance;
+        int _id;
+
+        public Account(int id, double balance)
+        {
+            this._id = id;
+            this.Balance = balance;
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+
+    public class AccountManager
+    {
+        Account FromAccount;
+        Account ToAccount;
+        double AmountToTransfer;
+
+        public AccountManager(Account fromAccount,
+            Account toAccount, double amountToTransfer)
+        {
+            this.FromAccount = fromAccount;
+            this.ToAccount = toAccount;
+            this.AmountToTransfer = amountToTransfer;
+        }
+
+        public void Transfer()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name+ " trying to acquire lock on "+ FromAccount.ID.ToString());
+            lock (FromAccount)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name+ " acquired lock on "+ FromAccount.ID.ToString());
+
+                Console.WriteLine(Thread.CurrentThread.Name + " suspended for 1 second");
+
+                Thread.Sleep(1000);
+
+                Console.WriteLine(Thread.CurrentThread.Name +" back in action and trying to acquire lock on "+ ToAccount.ID.ToString());
+
+                lock (ToAccount)
+                {
+                    FromAccount.Withdraw(AmountToTransfer);
+                    ToAccount.Deposit(AmountToTransfer);
+                }
+            }
+        }
+    }
+}
+    
---
+++ DeadLock1/Program.cs
@@ -0,0 +1,35 @@
+﻿using DeadLock1;
+using System;
+using System.Threading;
+namespace DeadLock1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Main Started");
+            Account accountA = new Account(101, 5000);
+            Account accountB = new Account(102, 3000);
+
+            AccountManager accountManagerA = new
+                AccountManager(accountA, accountB, 1000);
+            Thread T1 = new Thread(accountManagerA.Transfer);
+            T1.Name = "Task1";
+
+            AccountManager accountManagerB = new
+                AccountManager(accountB, accountA, 2000);
+            Thread T2 = new Thread(accountManagerB.Transfer);
+            T2.Name = "Task2";
+
+            T1.Start();
+            T2.Start();
+
+            T1.Join();
+            T2.Join();
+            Console.WriteLine("Main Completed");
+        }
+    }
+    }
+
+
+
