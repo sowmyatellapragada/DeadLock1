4b338b85c4b85e2f79f16cd351c75576c70fba68
sowmyatellapragada <63146223+sowmyatellapragada@users.noreply.github.com>
2021-01-06 15:35:23 +0530

    Update Program.cs

--- DeadLock1/Program.cs
+++ DeadLock1/Program.cs
@@ -11,13 +11,13 @@ namespace DeadLock1
             Account accountA = new Account(101, 5000);
             Account accountB = new Account(102, 3000);
 
-            AccountManager accountManagerA = new
-                AccountManager(accountA, accountB, 1000);
+            AccountManager accountManagerA = new  AccountManager(accountA, accountB, 10000);
+       
             Thread T1 = new Thread(accountManagerA.Transfer);
             T1.Name = "Task1";
 
-            AccountManager accountManagerB = new
-                AccountManager(accountB, accountA, 2000);
+            AccountManager accountManagerB = new AccountManager(accountB, accountA, 20000);
+                
             Thread T2 = new Thread(accountManagerB.Transfer);
             T2.Name = "Task2";
 
